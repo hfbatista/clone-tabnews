28e1291190d916c4f35807207408c77d90c98b8b
Henrique Batista <36209563+hfbatista@users.noreply.github.com>
2026-04-14 00:48:08 +0000

    add `lint:check`, `lint:fix` scripts and install prettier extension

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Gatinha, parabens por ser tão foda, voce é incrível e eu te amo, Se gostou da uma risadinha!!</h1>
+  return (
+    <h1>
+      Gatinha, parabens por ser tão foda, voce é incrível e eu te amo, Se gostou
+      da uma risadinha!!
+    </h1>
+  );
 }
 
 export default Home;
