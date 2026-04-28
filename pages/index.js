20bf1a2c03124ede8429784f717669b29eb67edb
Henrique Batista <36209563+hfbatista@users.noreply.github.com>
2026-04-28 23:56:55 +0000

    att mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 function Home() {
-  return (
-    <h1>
-      Gatinha, parabens por ser tão foda, voce é incrível e eu te amo, Se gostou
-      da uma risadinha!!
-    </h1>
-  );
-}
-
-function Home() {
   return console.log(
     "gatinha, voce é incrível e eu te amo, Se você me ama manda uma risadinha!!!",
   );
