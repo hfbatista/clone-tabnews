e7465872b07196e7861b64116e60b7ca6953cadb
Henrique Batista <36209563+hfbatista@users.noreply.github.com>
2026-04-28 23:53:50 +0000

    att mensagem

--- pages/index.js
+++ pages/index.js
@@ -7,4 +7,10 @@ function Home() {
   );
 }
 
+function Home() {
+  return console.log(
+    "gatinha, voce é incrível e eu te amo, Se você me ama manda uma risadinha!!!",
+  );
+}
+
 export default Home;
