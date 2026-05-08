cc4a99891ff52654a3afb41db6c11164c2101fdf
Henrique Batista <36209563+hfbatista@users.noreply.github.com>
2026-05-08 23:17:27 +0000

    fix .env file

--- infra/database.js
+++ infra/database.js
@@ -4,7 +4,7 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
