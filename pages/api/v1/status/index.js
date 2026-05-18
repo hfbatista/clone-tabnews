d798c1b1e39a7a07c668c3ce4d394d0d3150d8fb
Henrique Batista <36209563+hfbatista@users.noreply.github.com>
2026-05-18 22:32:01 +0000

    add validade status, max connections and connections number

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::integer FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    status: "200",
-    chave: "Eu sou acima da média segundo o Decampeões",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
   });
 }
 
